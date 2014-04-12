2b717f5ea64ea3b360b605ace5ed7ac06f647225
Sheri Bigelow <sheri@designsimply.com>
2014-04-12 17:37:29 -0400

    Change site description heading level to h4

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<h4 class="site-description"><?php bloginfo( 'description' ); ?></h4>
 		</div>
 
 		<?php get_search_form(); ?>
