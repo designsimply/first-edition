5846af8115430bdee3696db38154d3f54143d9a1
Sheri Bigelow <sheri@designsimply.com>
2014-04-12 16:40:50 -0400

    Move menu from header to sidebar

--- header.php
+++ header.php
@@ -27,12 +27,5 @@
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', 'first-edition' ); ?></h1>
-			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'first-edition' ); ?></a>
-
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
+		<?php get_search_form(); ?>
 	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
--- sidebar.php
+++ sidebar.php
@@ -6,6 +6,13 @@
  */
 ?>
 	<div id="secondary" class="widget-area" role="complementary">
+               <nav id="site-navigation" class="main-navigation" role="navigation">
+                       <h2 class="menu-toggle"><?php _e( 'Menu', 'first-edition' ); ?></h2>
+                       <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'first-edition' ); ?></a>
+
+                       <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+               </nav><!-- #site-navigation -->
+
 		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
 			<!-- This space intentionally left blank. No default widgets. -->
