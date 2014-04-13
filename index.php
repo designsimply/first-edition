022e5950e59cdfadbb1c2bc97e3181473fd7039d
Sheri Bigelow <sheri@designsimply.com>
2014-04-12 21:16:37 -0400

    Add container class to index and move single/page to index

--- index.php
+++ index.php
@@ -13,25 +13,36 @@
 
 get_header(); ?>
 
+	<div class="container">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
-			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+				<?php switch ( true ) :
+					case ( is_page() ) :
+						get_template_part( 'content', 'page' );
+						break;
+					case ( is_single() ) :
+						get_template_part( 'content', 'single' );
+						break;
+					default :
+						get_template_part( 'content', get_post_format() );
+						break;
+				endswitch;?>
+
+				<?php if ( is_single() || is_page() || is_attachment ) {
+					first_edition_post_nav();
+
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() ) :
+						comments_template();
+					endif;
+				} ?>
 
 			<?php endwhile; ?>
-
-			<?php first_edition_paging_nav(); ?>
 
 		<?php else : ?>
 
@@ -42,5 +53,7 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+	<?php get_sidebar(); ?>
+	</div><!-- .container -->
+
 <?php get_footer(); ?>
