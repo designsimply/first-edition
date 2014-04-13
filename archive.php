34db2348ec61ab662eb2bb81ab69313625c9072c
Sheri Bigelow <sheri@designsimply.com>
2014-04-12 21:16:04 -0400

    Add container class to archive pages

--- archive.php
+++ archive.php
@@ -9,6 +9,7 @@
 
 get_header(); ?>
 
+	<div class="container">
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -77,16 +78,9 @@ get_header(); ?>
 				?>
 			</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
 
 			<?php endwhile; ?>
 
@@ -101,5 +95,7 @@ get_header(); ?>
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
-<?php get_sidebar(); ?>
+	<?php get_sidebar(); ?>
+	</div><!-- .container -->
+
 <?php get_footer(); ?>
