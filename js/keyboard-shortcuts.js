67000f4243af284d37b4a30e7149d95ad10967ee
Sheri Bigelow <sheri@designsimply.com>
2014-05-01 08:20:06 -0400

    Add keyboard shortcut to focus on the site header search input

--- js/keyboard-shortcuts.js
+++ js/keyboard-shortcuts.js
@@ -4,13 +4,16 @@
  * Handles keyboard shortcuts for things like post and image navigation.
  */
 jQuery( document ).ready( function( $ ) {
-	$( document ).keydown( function( e ) {
+	$( document ).keyup( function( e ) {
 		var url = false;
 		if ( e.which == 37 ) {  // Left arrow key code
 			url = $( 'a[rel="prev"], .prev a' ).attr( 'href' );
 		}
 		else if ( e.which == 39 ) {  // Right arrow key code
 			url = $( 'a[rel="next"], .next a' ).attr( 'href' );
+		}
+		else if ( e.which == 191 ) {  // Forward slash key code
+			$( '.site-header .search-form .search-field' ).focus();
 		}
 		if ( url && ( !$( 'textarea, input' ).is( ':focus' ) ) ) {
 			window.location = url;
