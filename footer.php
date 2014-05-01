5db47d317dbcfdb6d2cba167fbc41c1e7af03926
Sheri Bigelow <sheri@designsimply.com>
2014-05-01 06:23:19 -0400

    Add back a default footer credit line

--- footer.php
+++ footer.php
@@ -11,7 +11,11 @@ $footer_text = get_theme_mod( 'first_edition_footer_text' );
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<?php if ( '' != $footer_text ) { echo $footer_text; } ?>
+			<?php if ( '' != $footer_text ) {
+				echo $footer_text;
+			} else {
+				printf( __( '%1$s was made with %2$s for %3$s', 'first-edition' ), '<a href="http://designsimply.com/wordpress/theme/first-edition/" rel="designer">First Edition WordPress Theme</a>', '<a href="http://underscores.me/" rel="inspiration">Underscores.me</a>', '<a href="http://wordpress.org/" rel="generator">WordPress</a>' );
+			} ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 
